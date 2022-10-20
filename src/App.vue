866b4a6729f8d7d61262d04f7b905feef0befe17
97jorx <97jorx@gmail.com>
2022-10-20 23:56:43 +0200

    [ADD] Axios get request fruits

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,41 @@
-<script setup>
-  
-  const name = "Vue dinámico";
-  const styleColor = "color: blue"
-  const array = [
-    {
-      name: 'Manzána',
-      price: '1€',
-      stock: '10'
-      
-    },
-    {
-      name: 'Plátano',
-      price: '0.75€',
-      stock: '15'
-    },
-    {
-      name: 'Coco',
-      price: '2€',
-      stock: '20',
-    },
-    {
-      name: 'Pera',
-      price: '0.8€',
-      stock: '12',
-    },
-    {
-      name: 'Naranja',
-      price: '0.6€',
-      stock: '11',
-    }
-
-  ];
-
-</script>
 <template>
-  <h1 :style="styleColor">Hello, {{ name.toUpperCase() }}!</h1>
   <ol>
-    <li v-for="fruta in array" 
-    :key="fruta.name">
-        {{ fruta.name }}
-       <ul>
-         <li>{{fruta.price}}</li>
-         <li>{{fruta.stock}}</li>
-        </ul> 
-      </li>
+    <li v-for="fruit in fruits" 
+      :key="fruit.name">
+      {{ fruit.name }}
+      <ul>
+        <li>{{fruit.id}}$</li>
+        <li>{{fruit.color}}$</li>
+        <li>{{fruit.count}}</li>
+      </ul> 
+    </li>
   </ol>
-</template>
+  </template>
+  <script>
+    import axios from 'axios'
+    
+  
+    export default {
+      data () {
+        return {
+          fruits:null
+        }
+      },
+      mounted() {
+         console.log('Hello World!')
+         this.getFruits()
+      },
+      methods: {
+        getFruits() {
+          console.log('Hello World! x2')
+          axios.get('https://www.fruitmap.org/api/trees')
+          .then( response => {
+              this.fruits = response.data
+          })
+          .catch( error => console.log(error))
+        }
+      }
+    }
+  </script>
+  
+  
